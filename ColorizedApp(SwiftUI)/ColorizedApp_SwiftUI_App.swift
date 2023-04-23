719327cca5c9daf9f826fff81e06ec31e1227b52
Manohin <111537419+Manohin@users.noreply.github.com>
2023-04-23 22:06:11 +0300

    Initial Commit

---
+++ ColorizedApp(SwiftUI)/ColorizedApp_SwiftUI_App.swift
@@ -0,0 +1,17 @@
+//
+//  ColorizedApp_SwiftUI_App.swift
+//  ColorizedApp(SwiftUI)
+//
+//  Created by Alexey Manokhin on 23.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct ColorizedApp_SwiftUI_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
